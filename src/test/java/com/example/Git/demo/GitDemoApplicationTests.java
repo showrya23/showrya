5c0b7cc0fc63bb3a052c1955b4ea587fd160a7ac
showrya23 <showryamj3@gmail.com>
2025-02-15 15:26:36 +0530

    Message

---
+++ src/test/java/com/example/Git/demo/GitDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Git.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
