dcb6b62c1923c819ecdf9bf1e2231c485a30b3b5
showrya23 <showryamj3@gmail.com>
2025-02-16 10:46:12 +0530

    message

---
+++ src/main/java/com/example/Git/demo/product.java
@@ -0,0 +1,22 @@
+package com.example.Git.demo;
+
+public class product {
+    private int productId;
+    private String productName;
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public int getProductId() {
+        return productId;
+    }
+
+    public void setProductId(int productId) {
+        this.productId = productId;
+    }
+}
